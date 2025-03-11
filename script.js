12593c9d6ba97714eba39710d30e2e204596c047
Saiokokun <90825412+saiokokun@users.noreply.github.com>
2025-03-11 14:54:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let cube = document.querySelector(".cube");
+    let isRotating = true;
 
+    cube.addEventListener("mouseover", function () {
+        isRotating = false;
+        cube.style.animation = "none";
+    });
+
+    cube.addEventListener("mouseleave", function () {
+        isRotating = true;
+        cube.style.animation = "rotate 6s infinite linear";
+    });
+});
